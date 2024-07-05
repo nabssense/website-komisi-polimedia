c7bde0b14570827c2ff16750bb7241962a604254
nabssense <nabssense@gmail.com>
2024-07-06 04:56:21 +0700

    Update Tampilan5

--- resources/js/animation copy.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".hover-animation").forEach(function(element) {
-    element.addEventListener("mouseenter", function() {
-        this.querySelector('.moving-animation').classList.add("movingText");
-    });
-});
--- resources/js/togglePopupShow.js
+++
@@ -1,9 +0,0 @@
-function togglePopUpShow(popupId) {
-    var popup = document.getElementById(popupId);
-    if (popup.classList.contains('hidden')) {
-        popup.classList.remove('hidden');
-    } else {
-
-        popup.classList.add('hidden');
-    }
-}
