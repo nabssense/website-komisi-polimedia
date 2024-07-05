d4f66eb9494745c885a2f96fc8bfd774847111da
nabssense <nabssense@gmail.com>
2024-07-05 16:15:14 +0700

    percobaan ke server7

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,20 +26,6 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-
-            // Replace where you use Faker with Laravel's factory function
-            'fullname' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'password' => $this->faker->password(),
-            'nim' => $this->faker->phoneNumber(),
-            'user_type' => $this->faker->name(),
-            'edu_program' => $this->faker->name(),
-            'status' => $this->faker->name(),
-
-            // 'email_verified_at' => now(),
-            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            // 'remember_token' => Str::random(10),
-
         ];
     }
 
