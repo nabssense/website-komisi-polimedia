1bff3950943a3c6a61db55c0e4988d1ca2aeec6e
nabssense <nabssense@gmail.com>
2024-07-10 22:53:22 +0700

    testing Final Bgt fix user profile

--- resources/views/partials/forum-diskusi/diskusi-terbantu.blade.php
+++ resources/views/partials/forum-diskusi/diskusi-terbantu.blade.php
@@ -14,7 +14,7 @@
                     {{-- Profil --}}
                     <div class="w-full justify-start items-center gap-2 inline-flex">
                         <div class="flex-none" href="{{ route('user.profile') }}">
-                            <img src="{{ strpos(auth()->user()->profile_picture, 'https://ui-avatars.com') === 0 ? auth()->user()->profile_picture : Storage::url($user->profile_picture) }}"
+                            <img src="{{ strpos($discussion->user->profile_picture, 'https://ui-avatars.com') === 0 ? $discussion->user->profile_picture : Storage::url($user->profile_picture) }}"
                                 alt="{{ $discussion->user->fullname }}" class="rounded-full w-9 lg:w-12">
                         </div>
                         <div class="w-full h-fit flex-col justify-center items-start inline-flex Body1">
